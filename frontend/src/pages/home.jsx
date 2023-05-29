a9250802a6a5c31ad59337ff295308215facae6c
Chisom Ibekwe <lucasbolt700@gmail.com>
2023-05-29 05:55:24 +0100

    latest update (frontend)

---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,12 @@
+import "../App.css";
+import Navbar from "../components/navbar";
+
+function Home() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default Home;
